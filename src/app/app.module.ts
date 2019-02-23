625ff95920eb348c5d51f103ab3f5d89701e1383
florimondmanca <florimond.manca@gmail.com>
2019-02-23 20:42:55 +0100

    format app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { HttpClientModule } from "@angular/common/http";
 
 import { AppComponent } from "./app.component";
 import { CourseListComponent } from "./course-list/course-list.component";
+import { FooComponent } from './foo/foo.component';
 
 @NgModule({
-  declarations: [AppComponent, CourseListComponent],
+  declarations: [AppComponent, CourseListComponent, FooComponent],
   imports: [BrowserModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent]
